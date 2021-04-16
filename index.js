81327fb6a6919bbe0a697f539d5d65662f721b3c
delurd <47378811+delurd@users.noreply.github.com>
2021-04-16 10:04:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-function btnClick(nomer) {
-  const pageBlur = document.querySelector(".pageBlur");
-  pageBlur.classList.toggle("active");
+ //Mengetahui jumlah card
+  var items = document.getElementsByClassName("detailCard");
+  console.log("items ="+items.length);
+  var durasi = 0;
+  
+  //Menganimasi saat halaman dibuka
+  $(document).ready(function(){
+      $(".row").fadeIn(3000);
 
-  // const detailPage = document.querySelector('.detailCard');
-  // detailPage.classList.toggle('active');
+      for(i=0;i<items.length;i++){
+          $("#card"+i).fadeIn(durasi+=1000);
+          console.log("fade "+i);
+      }
+  })
+  
+  //Memanggil efect Blur
+  $(document).ready(function(){
+      $(".btn").click(function(){
+          $(".page-1").toggleClass("pageBluractive");
+      })
+      $(".close").click(function(){
+          $(".page-1").toggleClass("pageBluractive");
+          
+      })
+  })
 
-  var boten = document.getElementById("detailCard" + nomer);
-  boten.style.display = "block";
-}
-function btnClose(nomer) {
-  var cls = document.getElementById("detailCard" + nomer);
-  cls.style.display = "none";
 
-  const pageBlur = document.querySelector(".pageBlur");
-  pageBlur.classList.toggle("active");
-}
+  //Memanggil detailcard sesuai nomer
+  function btnClick(nomer) {
+      $(document).ready(function(){
+          $("#detailCard"+nomer).slideToggle(200);
+      })
+  }
+  function btnClose(nomer) {
+      $(document).ready(function(){
+          $("#detailCard"+nomer).slideToggle(300);
+      })
+  }
